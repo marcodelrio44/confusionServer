cb3bee525d1061db75a821123bcf7abb0067edc5
Marco Delrio <marcodelrio44@gmail.com>
2022-06-18 19:53:34 +0200

    Logout api

--- routes/users.js
+++ routes/users.js
@@ -52,4 +52,17 @@ router.post('/login', passport.authenticate('local',{session: false}), (req, res
   res.json({success: true,token:token, status: 'You are successfully logged in!'});
 });
 
+router.get('/logout', (req, res) => {
+  if (req.session) {
+      req.session.destroy();
+      res.clearCookie('session-id');
+      res.redirect('/');
+  }
+  else {
+      var err = new Error('You are not logged in!');
+      err.status = 403;
+      next(err);
+  }
+});
+
 module.exports = router;
